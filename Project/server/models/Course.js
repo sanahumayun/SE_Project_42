fb197c6bf91964fcf0d6589af539dfec659a6f4c
sanahumayun <26100273@lums.edu.pk>
2025-04-07 18:41:23 +0500

    Revert "Course and Assignment Management"
    This reverts commit 5b811b2214f45ad41bc72bbd7b3ec0429d7dce1b.

--- Project/server/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  tutorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Course', courseSchema);
