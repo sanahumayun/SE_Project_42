092bc31bb07ed448243bdd2c313cefb43f8b64ae
arqam-21 <sajjadarqam21122@gmail.com>
2025-04-25 20:35:39 +0500

    DB to store messages

---
+++ Project/server/models/Message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  chatRoomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ChatRoom',
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
