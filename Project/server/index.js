a4979a21e4ec8e83d547313844a8f4cd27545396
sanahumayun <26100273@lums.edu.pk>
2025-05-05 17:07:51 +0500

    fix

--- Project/server/index.js
+++ Project/server/index.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://se-project-42-frontend.vercel.app',
+  credentials: true
+}));
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
